d7efcfedf4b21045db01cfe4996270d6475fbd66
tswya <617923986@qq.com>
2023-01-21 10:25:21 +0800

    report finished

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -8,18 +8,57 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-      <div id="main" style="width: 600px; height: 400px"></div>
+      <div id="main" style="width: 750px; height: 400px"></div>
     </el-card>
   </div>
 </template>
 
 <script>
 import * as echarts from 'echarts'
+import _ from 'lodash'
+
 export default {
-  data() {},
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
   created() {},
-  mounted() {
+  async mounted() {
     var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败！')
+    }
     var option = {
       title: {
         text: 'ECharts 入门示例'
@@ -37,7 +76,8 @@ export default {
         }
       ]
     }
-    myChart.setOption(option)
+    const result = _.merge(res.data, this.options)
+    myChart.setOption(result)
   },
   methods: {}
 }
